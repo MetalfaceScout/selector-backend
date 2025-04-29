d3e177b11e7e9f1b68d3802d3c6e516835ce0cf9
MetalfaceScout <logan.mathis@utahtech.edu>
2025-04-29 11:30:54 -0600

    Basic functionality acheived

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::{collections::HashMap, env::{self}, u64};
+use core::panic;
+use std::{collections::HashMap, env::{self}, f64::MAX, u64};
 
 use clap::Parser;
 use dotenvy::dotenv;
 use postgres::Client;
 use native_tls::TlsConnector;
 use postgres_native_tls::MakeTlsConnector;
+use rand::{rng, seq::IndexedRandom};
 
 /// The backend of the Team Selector by Metalface - Intended to be used by the website
 #[derive(Parser, Debug)]
@@ -73,18 +75,20 @@ enum MVPCalculationMode {
     Last_N_Mean,
 }
 
+#[derive(Clone)]
 struct PlayerStats {
     player_id: u64,
-    commander_stats: PostionStats,
-    heavy_stats: PostionStats,
-    scout_stats: PostionStats,
-    ammo_stats: PostionStats,
-    medic_stats: PostionStats,
-    total_games_played: u64
+    player_name: String,
+    commander_stats: PositionStats,
+    heavy_stats: PositionStats,
+    scout_stats: PositionStats,
+    ammo_stats: PositionStats,
+    medic_stats: PositionStats,
+    //total_games_played: u64
 }
 
-#[derive(Clone)]
-struct PostionStats {
+#[derive(Clone, Copy)]
+struct PositionStats {
     mvp: f64,
     hit_diff: f64,
     games_played: u64
@@ -118,7 +122,18 @@ fn retrieve_stats(player_id : u64, mvp_calc_method : MVPCalculationMode) -> Play
 
     let params = set_database_params();
     let mut db_client = create_db_client(params);
-    let mut all_games_played = 0u64;
+    //let mut all_games_played = 0u64;
+
+    let query = format!("SELECT mvp_points, hit_diff, position, player_name FROM scorecards WHERE player_id = {}", player_id);
+    let results = db_client.query(&query, &[]).expect("Failed to retrieve player stats");
+
+    let player_name;
+
+    if results.len() == 0 {
+        player_name = String::from("Unknown Player");
+    } else {
+        player_name = results[0].get(3);
+    }
 
     let mut stats = HashMap::new();
 
@@ -126,40 +141,50 @@ fn retrieve_stats(player_id : u64, mvp_calc_method : MVPCalculationMode) -> Play
 
         let position_string = set_position_from_index(position);
 
-        let query = format!("SELECT mvp_points, hit_diff FROM scorecards WHERE id = {} AND position = '{}'", player_id, position_string);
-        let results = db_client.query(&query, &[]).expect("Failed to retrieve player stats");
-
         let mut all_mvps: Vec<f64> = Vec::new();
         let mut all_hit_diffs: Vec<f64> = Vec::new();
         let mut game_count: u64 = 0;
 
         for row in &results {
-            all_mvps.push(row.get(0));
-            all_hit_diffs.push(row.get(1));
-            game_count += 1;
+            let pos: String = row.get(2);
+            if pos == set_position_from_index(position) {
+                all_mvps.push(row.get(0));
+                all_hit_diffs.push(row.get(1));
+                game_count += 1;
+            }
         }
 
-        let mvp_stats = calculate_stats(all_mvps, mvp_calc_method.clone());
-        let hit_diff_stats = calculate_stats(all_hit_diffs, mvp_calc_method.clone());
+        let mut mvp_stats = calculate_stats(all_mvps, mvp_calc_method.clone());
 
-        let position_stats = PostionStats {
+        if mvp_stats == -1.0 {
+            mvp_stats = 7.0;
+        }
+
+        let mut hit_diff_stats = calculate_stats(all_hit_diffs, mvp_calc_method.clone());
+        
+        if hit_diff_stats == -1.0 {
+            hit_diff_stats = 1.0;
+        }
+
+        let position_stats = PositionStats {
             hit_diff: hit_diff_stats,
             mvp: mvp_stats,
             games_played: game_count
         };
 
         stats.insert(position_string, position_stats);
-        all_games_played += game_count;
+        //all_games_played += game_count;
     }
 
     let final_stats = PlayerStats {
         player_id: player_id,
+        player_name: player_name,
         commander_stats: stats.get("Commander").unwrap().clone(),
         heavy_stats: stats.get("Heavy Weapons").unwrap().clone(),
         scout_stats: stats.get("Scout").unwrap().clone(),
         ammo_stats: stats.get("Ammo Carrier").unwrap().clone(),
         medic_stats: stats.get("Medic").unwrap().clone(),
-        total_games_played: all_games_played,
+        //total_games_played: all_games_played,
     };
 
     return final_stats;
@@ -201,10 +226,10 @@ where
 fn calculate_stats(stats : Vec<f64>, mvp_calc_method : MVPCalculationMode) -> f64 {
     match mvp_calc_method {
         MVPCalculationMode::Median => {
-            return find_median(&stats).unwrap_or(7.0);
+            return find_median(&stats).unwrap_or(-1.0);
         }
         MVPCalculationMode::Mean => {
-            return find_mean(&stats).unwrap_or(7.0);
+            return find_mean(&stats).unwrap_or(-1.0);
         }
         MVPCalculationMode::Last_N_Mean => {
             todo!("N mode support is not done.")
@@ -248,19 +273,19 @@ fn main() {
 
     match args.game_type {
         GameType::Sm5_12_Player => {
-            expected_player_count = 12;
+            expected_player_count = 6 * args.team_count as u64;
         }
         
         GameType::Sm5_10_Player => {
-            expected_player_count = 10;
+            expected_player_count = 5 * args.team_count as u64;
         }
 
         GameType::Sm5_14_Player => {
-            expected_player_count = 14;
+            expected_player_count = 7 * args.team_count as u64;
         }
 
         GameType::Sm5_QueenBee => {
-            expected_player_count = 8;
+            expected_player_count = 4 * args.team_count as u64;
         }
     }
 
@@ -278,9 +303,12 @@ fn main() {
 
 }
 
+#[derive(Clone)]
 struct PlayerSlot {
     position : u64,
-    player_id : i64
+    player_id : i64,
+    smvp: f64,
+    player_name: String
 }
 
 fn create_game(game : GameType, team_count : u8) -> Vec<Vec<PlayerSlot>> {
@@ -295,7 +323,7 @@ fn create_game(game : GameType, team_count : u8) -> Vec<Vec<PlayerSlot>> {
                 for player in &postion_array {
                     player_vec.push(
                         PlayerSlot { 
-                            position: *player, player_id: -1 }
+                            position: *player, player_id: -1, smvp: -1.0, player_name : "".to_string() }
                     );
                 }
                 final_vec.push(player_vec);
@@ -310,17 +338,94 @@ fn create_game(game : GameType, team_count : u8) -> Vec<Vec<PlayerSlot>> {
     }
 }
 
-fn calculate_advanced(game : Vec<Vec<PlayerSlot>>, players : Vec<PlayerStats>, modifiers_position : &Option<Vec<u64>>, modifiers_team : &Option<Vec<u64>>) {
-    modifiers_position;
-    modifiers_team;
+fn calculate_advanced(mut game : Vec<Vec<PlayerSlot>>, mut players : Vec<PlayerStats>, _modifiers_position : &Option<Vec<u64>>, _modifiers_team : &Option<Vec<u64>>) {
 
-    // Choose a random position from a random team
+    // Choose a random slot from a random team
     // Assign a player to that
     // Now look for the closest matching player from playerstats
     // Assign that player to the other team
 
-    for player in players {
-        println!("Player {} with commander mvp {} and hit diff {}", player.player_id, player.commander_stats.mvp, player.commander_stats.hit_diff);
+    let mut rng = rng();
+
+    // Run once per slot on one team
+    for slot_index in 0..game.clone().first().unwrap().len() {
+
+        //let slot_index = rand::random_range(0..game[0].len());
+
+        let player = players.choose(&mut rng).unwrap().clone();
+        players.retain(|p| p.player_id != player.player_id);
+
+        game[0][slot_index].player_id = player.player_id.try_into().unwrap();
+
+        // The position of the retrieved slot
+        let position = game[0][slot_index].position;
+
+        // Fetch stats of that position
+        let stats = retrieve_stat_block_from_position(position, player.clone());
+
+        // Match a player for each opposing team (usually just 1)
+        for team_match in 1..game.len() {
+            // Match the next closest player's position stats
+            let smvp = calculate_smvp(stats);
+                        
+            game[0][slot_index].smvp = smvp;
+            game[0][slot_index].player_name = player.player_name.clone();
+
+            // At the end of this next for loop, should have the closest player in mvp
+            let mut min_diff = MAX;
+            let mut diff_index: usize = 0;
+
+            let mut other_smvp_chosen: f64 = 0.0;
+            let mut other_name_chosen = String::from("");
+
+
+            for (index, other_player) in players.iter().enumerate() {
+                let p_stats = retrieve_stat_block_from_position(position, other_player.clone());
+                let other_smvp = calculate_smvp(p_stats);
+                let other_name = other_player.player_name.clone();
+                let diff = (smvp - other_smvp).abs();
+
+
+                if diff < min_diff {
+                    min_diff = diff;
+                    diff_index = index;
+                    other_smvp_chosen = other_smvp;
+                    other_name_chosen = other_name;
+                }
+            }
+
+            let matched_player = players.get(diff_index).unwrap().clone();
+            players.retain(|p| p.player_id != matched_player.player_id);
+
+            game[team_match][slot_index].player_id = matched_player.player_id as i64;
+            game[team_match][slot_index].smvp = other_smvp_chosen;
+            game[team_match][slot_index].player_name = other_name_chosen;
+        }
+    
     }
 
+    for (i, team) in game.iter().enumerate() {
+        println!("Team {}", i);
+        for slot in team {
+            println!("Player ID: {}, Player Position: {}, Player SMVP: {}, Player Name: {}", slot.player_id, set_position_from_index(slot.position), slot.smvp, slot.player_name);
+        }
+    }
+
+}
+
+
+/// Calculate the magic number that the selector will use to rank players. The way to calculate this is up for debate
+fn calculate_smvp(stats : PositionStats) -> f64 {
+    return (stats.hit_diff * stats.mvp) + (stats.games_played as f64 / 100.0);
+}
+
+fn retrieve_stat_block_from_position(postion : u64, player : PlayerStats) -> PositionStats {
+    match postion {
+        0 => {return player.commander_stats}
+        1 => {return player.heavy_stats}
+        2 => {return player.scout_stats}
+        3 => {return player.ammo_stats}
+        4 => {return player.medic_stats}
+        _ => {panic!("This is wrong.")}
+    }
 }
